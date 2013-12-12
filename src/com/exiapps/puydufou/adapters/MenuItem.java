0066281e565f8e71dc6c451436bfbf7795d8483b
nicolaspercheron <nicolas.percheron@viacesi.fr>
2013-12-12 16:36:00 +0100

    b

--- src/com/exiapps/puydufou/adapters/MenuItem.java
+++ src/com/exiapps/puydufou/adapters/MenuItem.java
@@ -1,42 +1,41 @@
 package com.exiapps.puydufou.adapters;
 
-import com.exiapps.puydufou.R;
-
 import android.content.Context;
+
+import com.exiapps.puydufou.R;
 
 public enum MenuItem {
 
 	MAP, SCHEDULE, INFO, HISTORIC, RESTAURANTS, SHOPS;
 
-
 	public String getString(Context context) {
-		
+
 		String string = "";
-		
+
 		switch (this) {
-		
-			case MAP:
-				string = context.getString(R.string.map);
+
+		case MAP:
+			string = context.getString(R.string.map);
 			break;
-			case SCHEDULE:
-				string = context.getString(R.string.schedule);
+		case SCHEDULE:
+			string = context.getString(R.string.schedule);
 			break;
-			case INFO:
-				string = context.getString(R.string.info);
+		case INFO:
+			string = context.getString(R.string.info);
 			break;
-			case HISTORIC:
-				string = context.getString(R.string.historic);
-			break;		
-			case RESTAURANTS:
-				string = context.getString(R.string.restaurants);
+		case HISTORIC:
+			string = context.getString(R.string.historic);
 			break;
-			case SHOPS:
-				string = context.getString(R.string.shops);
-			break;		
+		case RESTAURANTS:
+			string = context.getString(R.string.restaurants);
+			break;
+		case SHOPS:
+			string = context.getString(R.string.shops);
+			break;
 		default:
 			break;
 		}
-		
+
 		return string;
 	}
 }
