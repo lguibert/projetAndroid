0f037902acd5c63804898728470bd1ad574a3ab0
Lucas Guibert <lucas.guibert@viacesi.fr>
2013-12-12 15:11:56 +0100

    padada

--- src/com/exiapps/puydufou/fragment/ScheduleFragment.java
+++ src/com/exiapps/puydufou/fragment/ScheduleFragment.java
@@ -2,13 +2,6 @@ package com.exiapps.puydufou.fragment;
 
 import java.util.List;
 
-
-
-
-
-
-
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentTransaction;
@@ -31,6 +24,7 @@ import com.exiapps.puydufou.adapters.SpectacleAdapter;
 import com.exiapps.puydufou.model.OnReceiveListener;
 import com.exiapps.puydufou.model.SpectacleManager;
 import com.exiapps.puydufou.model.entities.Spectacle;
+import com.exiapps.puydufou.view.FocusShowActivity;
 import com.exiapps.puydufou.view.MainActivity;
 
 public class ScheduleFragment extends AbstractFragment implements OnReceiveListener{
@@ -54,30 +48,11 @@ public class ScheduleFragment extends AbstractFragment implements OnReceiveListe
 		listView.setOnItemClickListener(new OnItemClickListener() {
 			@Override
 			public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-				//Toast.makeText(getActivity().getApplicationContext(), "coucou "+position, Toast.LENGTH_LONG).show();	
-				
-				//view = inflater.inflate(R.layout.fragment_focus_show, container, false);
-				
-								
-				
-				
-				AbstractFragmentPagerAdater pagerAdapter = ((MainActivity)getActivity()).getmPagerAdapter();
-				
-				for (int i = 0; i < pagerAdapter.getCount(); i++) {
-					getActivity().getSupportFragmentManager().beginTransaction().remove(pagerAdapter.getItem(i)).commit();
-				}
-				
-				pagerAdapter = new InformationPagerAdapter(getActivity().getSupportFragmentManager(), getActivity());
-				
-	
-				
-				((MainActivity)getActivity()).getPager().removeAllViews();
-				((MainActivity)getActivity()).getPager().setAdapter(pagerAdapter);
-				
-				
-			}
-			
-			
+				Intent i = new Intent(getActivity().getApplicationContext(), FocusShowActivity.class);	
+				i.putExtra("id", ((Spectacle) listView.getAdapter().getItem(position)).getId());
+				System.out.println("ID: "+((Spectacle) listView.getAdapter().getItem(position)).getId());
+				startActivity(i);
+			}			
 		});
 		
 		this.refresh();
